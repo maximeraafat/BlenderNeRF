4db1d008d2df379f2e54ccd872c8d1c1023745b4
Maxime <maxime.raafat@yahoo.com>
2023-01-09 10:38:15 +0100

    sphere_camera must be perspective

--- blender_nerf_operator.py
+++ blender_nerf_operator.py
@@ -7,6 +7,7 @@ import bpy
 # global addon script variables
 OUTPUT_TRAIN = 'images_train'
 OUTPUT_TEST = 'images_test'
+CAMERA_NAME = 'BlenderNeRF Camera'
 
 
 # blender nerf operator parent class
@@ -107,6 +108,7 @@ class BlenderNeRF_Operator(bpy.types.Operator):
         assert method == 'SOF' or method == 'TTC' or method == 'COS'
 
         camera = scene.camera
+        sphere_camera = scene.objects[CAMERA_NAME]
         train_camera = scene.camera_train_target
         test_camera = scene.camera_test_target
 
@@ -121,6 +123,9 @@ class BlenderNeRF_Operator(bpy.types.Operator):
 
         if method == 'TTC' and not (train_camera.data.type == 'PERSP' and test_camera.data.type == 'PERSP'):
            error_messages.append('Only perspective cameras are supported!')
+
+        if method == 'COS' and CAMERA_NAME in scene.objects.keys() and not sphere_camera.data.type == 'PERSP':
+            error_messages.append('BlenderNeRF Camera must remain a perspective camera!')
 
         if (method == 'SOF' and sof_name == '') or (method == 'TTC' and ttc_name == '') or (method == 'COS' and cos_name == ''):
             error_messages.append('Dataset name cannot be empty!')
